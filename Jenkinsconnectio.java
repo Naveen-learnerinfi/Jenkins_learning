b6eaf7f8c22a0c525b20a41844ce72d86512ece1
Naveen-learnerinfi <115209879+Naveen-learnerinfi@users.noreply.github.com>
2023-03-07 21:33:50 +0530

    file updated

--- Jenkinsconnectio.java
+++ Jenkinsconnectio.java
@@ -2,6 +2,7 @@ public class Jenkinsconnection {
 public static void main(String []args) {
 System.out.println("i am gonna connect your with git repository in jenkins ");
 System.out.println(" whenever i am pushed a build gonna automatically run in jenkins ");
+System.out.println(" hi i am from git");
 
 }
 
