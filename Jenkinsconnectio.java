8614e4ea8ded90a72f832cbf89801f701912f782
Naveen-learnerinfi <115209879+Naveen-learnerinfi@users.noreply.github.com>
2023-03-07 20:37:18 +0530

    c=

---
+++ Jenkinsconnectio.java
@@ -0,0 +1,8 @@
+public class Jenkinsconnection {
+public static void main(String []args) {
+System.out.println("i am gonna connect your with git repository in jenkins ");
+System.out.println(" whenever i am pushed a build gonna automatically run in jenkins ");
+
+}
+
+}
